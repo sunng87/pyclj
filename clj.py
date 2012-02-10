bae457c3879ed7d495c47ee5016ce7b789dce7ee
Sun Ning <classicning@gmail.com>
2012-02-10 21:47:06 +0800

    minor changesa

--- clj.py
+++ clj.py
@@ -1,4 +1,46 @@
+# Copyright (C) 2012 Sun Ning<classicning@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# clojure literals => python types
+#
+# clojure vector [1 2 3 4] => python list [1 2 3 4] *coll
+# clojure list '(1 2 3 4) => python list [1 2 3 4] *coll
+# clojure set #{1 2 3 4} => python set set(1 2 3 4) *coll
+# clojure map {:a 1 :b 2} => python dict dict(a=1,b=2) *coll
+# clojure string "a" => python string "a"
+# clojure character \a => python string "a"
+# clojure keyword :a => python string "a"
+# clojure integer 123 => python integer 123
+# clojure float 12.3 => python float 12.3
+# clojure boolean true => python boolean true
+# clojure nil => python None 
+#
+
+__all__ = ["dump", "dumps", "load", "loads"]
+
+
+import re
+    
 def dump(obj, fp):
     pass
 
