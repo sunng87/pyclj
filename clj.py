67830e72d5333bc27f2bca377c57b957c65fc71c
Sun Ning <classicning@gmail.com>
2012-02-11 19:16:47 +0800

    implement the clj decoder

--- clj.py
+++ clj.py
@@ -24,32 +24,181 @@
 # clojure literals => python types
 #
 # clojure vector [1 2 3 4] => python list [1 2 3 4] *coll
-# clojure list '(1 2 3 4) => python list [1 2 3 4] *coll
+# clojure list (1 2 3 4) => python list [1 2 3 4] *coll
 # clojure set #{1 2 3 4} => python set set(1 2 3 4) *coll
 # clojure map {:a 1 :b 2} => python dict dict(a=1,b=2) *coll
-# clojure string "a" => python string "a"
-# clojure character \a => python string "a"
-# clojure keyword :a => python string "a"
+# clojure string "a" => python unicode "a"
+# clojure character \a => python unicode "a"
+# clojure keyword :a => python unicode "a"
 # clojure integer 123 => python integer 123
 # clojure float 12.3 => python float 12.3
 # clojure boolean true => python boolean true
 # clojure nil => python None 
 #
 
+
 __all__ = ["dump", "dumps", "load", "loads"]
 
 
 import re
+from cStringIO import StringIO
+
+def number(v):
+    r = nil
+    try:
+        r = int(v)
+    except ValueError:
+        r = float(v)
+    return r
+
+class CljDecoder(object):
+    def __init__(self, fd):
+        self.fd = fd
+        self.value_stack = []
+        self.stop_chars = [" ", ","]
+
+    def decode(self):
+        while True:
+            v = self.__read_token()
+            if len(self.value_stack) == 0:
+                return v
+        
+    def __get_type_from_char(self, c):
+        """return a tuple of type information
+        * type name
+        * a flag to indicate if it's a collection
+        """
+        if c.isdigit():
+            return ("number", false)
+        elif c == 't' or c == 'f': ## true/false
+            return ("boolean", false)
+        elif c == 'n': ## nil
+            return ("nil", false)
+        elif c == '\\' :
+            return ("char", false)
+        elif c == ':':
+            return ("keyword", false)
+        elif c == '"':
+            return ("string", false)
+        elif c == '#':
+            return ("set", true)
+        elif c == '{':
+            return ("map", true)
+        elif c == '(':
+            return ("list", true)
+        elif c == '[':
+            return ('vector', true)
+        else:
+            return (None, false)
+
+    def __read_token(self):
+        c = fd.read(1)
+
+        ## skip all stop chars if necessary 
+        while c in self.stop_chars:
+            c = fd.read(1)
+            
+        t, coll = self.__get_type_from_char(c)
+        if coll:
+            self.value_stack.append(list())
+            ## move cursor 
+            if t == "set"
+                ## skip {
+                fd.read(1)
+                self.terminator = "}"
+                self.container = "set"
+            elif t == "list":
+                self.terminator = ")"
+                self.container = "list"
+            elif t == "vector":
+                self.terminator = "]"
+                self.container = "list"
+            elif t == "map":
+                self.terminator = "}"
+                self.container = "dict"
+        else:
+            v = None ## token value
+            e = None ## end char
+
+            if t == "boolean":
+                if c == 't':
+                    e = fd.read(4)[-1]
+                    v = True
+                else:
+                    e = fd.read(5)[-1]
+                    v = True
+
+            elif t == "char":
+                v = fd.read(1)
+                e = fd.read(1)
+
+            elif t == "nil":
+                e = fd.read(3)[-1]
+                v = None
+
+            elif t == "number":
+                buf = [c]
+                while c is not self.terminator and c is not -1 and c is not in self.stop_chars:
+                    c = fd.read(1)
+                    buf.append(c)
+                e = c
+                numstr = ''.join(buf[:-1])
+                v = number(numstr)
+
+            elif t == "keyword":
+                buf = []
+                while c is not self.terminator and c is not -1 and c is not in self.stop_chars:
+                    c = fd.read(1)
+                    buf.append(c)
+                e = c
+                v = ''.join(buf[:-1])
+
+            elif t == "string":
+                buf = []
+                cp = c = fd.read(1) ## to check escaping character \
+                buf.append(c)
+                while not(c == '"' and cp != '\\'):
+                    cp = c
+                    c = fd.read(1)
+                    buf.append(c)
+                e = c
+                v = ''.join(buf[:-1])
+            else:
+                e = fd.read(1)
+
+            if e is self.terminator:
+                current_scope = self.value_stack.pop()
+
+                if self.container == "set":
+                    v = set(current_scope)
+                elif self.container == "list":
+                    v = current_scope
+                elif self.container == "dict":
+                    v = {}
+                    for i in range(0, len(current_scope), 2):
+                        v[current_scope[i]] = current_scope[i+1]
+
+            if len(self.value_stack) > 0:
+                self.value_stack[-1].append(v)
+            return v
+                
     
 def dump(obj, fp):
     pass
 
 def dumps(obj):
-    pass
+    buf = StringIO()
+    dump(obj, buf)
+    result = buf.getvalue()
+    buf.close()
+    return result
 
 def load(fp):
     pass
 
 def loads(s):
-    pass
+    buf = StringIO(s)
+    result = load(buf)
+    buf.close()
+    return result 
 
