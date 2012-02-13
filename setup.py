ad7f089ec334baedacafd36bf549729b87acffd6
Sun Ning <classicning@gmail.com>
2012-02-13 20:50:28 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
+long_desc=open('README.rst','r').read()
+
 setup(name="pyclj",
       version="0.1.2",
       author="Sun Ning",
       author_email="sunng@about.me",
       description="clojure literal reader and writer for python",
+      long_description=long_desc,
       url="http://github.com/sunng87/pyclj",
       license='mit',
       classifiers=['Development Status :: 3 - Alpha',
