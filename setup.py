44e41bbf4624574e660b42fef58bc8fcd7cdc2ff
Sunng <classicning@gmail.com>
2012-03-22 12:14:14 +0800

    fix packaging error

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ except ImportError:
 long_desc=open('README.rst','r').read()
 
 setup(name="pyclj",
-      version="0.1.8.1",
+      version="0.1.8.2",
       author="Sun Ning",
       author_email="sunng@about.me",
       description="clojure literal reader and writer for python",
       long_description=long_desc,
       url="http://github.com/sunng87/pyclj",
       license='mit',
+      py_modules=['clj'],
       classifiers=['Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
